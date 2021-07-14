1701f716654f871313b96b6948272779be2f2f6c
RaVeR76 <leefoster76@live.co.uk>
2021-07-14 19:57:09 +0000

    Add profile.html & link to session user

--- app.py
+++ app.py
@@ -46,6 +46,8 @@ def signup():
         # Utilise Session Cookie
         session["user"] = request.form.get("username").lower()
         flash("Sign Up Successful !")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("signup.html")
 
 
@@ -61,7 +63,10 @@ def login():
             if check_password_hash(
                     known_user["password"], request.form.get("password")):
                         session["user"] = request.form.get("username").lower()
-                        flash("Welcome, {}".format(request.form.get("username")))
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -73,6 +78,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # pull the session user's username from the database
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
