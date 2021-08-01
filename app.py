affe0b34edbbb6282678b43e4f6e24799be0f659
RaVeR76 <leefoster76@live.co.uk>
2021-08-01 19:42:51 +0000

    Amend the main vinyl page title

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def login():
             if check_password_hash(
                 known_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(
+                    flash("Welcome {}".format(
                         request.form.get("username")))
                     return redirect(url_for("profile"))
             else:
